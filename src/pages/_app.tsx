9095610169a26b8206ac80251e92f4570c7d303c
= <hotalog@hota1024.com>
2021-11-11 17:53:43 +0900

    💄remove margins of html and body

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,20 @@
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+
+      <style global jsx>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          background: #fefefe;
+        }
+      `}</style>
+    </>
+  )
 }
 
 export default MyApp
