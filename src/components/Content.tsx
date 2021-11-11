aafdddb4c55f311684b6df61471a27821b0f3c8f
= <hotalog@hota1024.com>
2021-11-11 18:27:42 +0900

    💄add Container and Content components

---
+++ src/components/Content.tsx
@@ -0,0 +1,63 @@
+/**
+ * ContentJustify type.
+ */
+export type ContentJustify = 'left' | 'center' | 'right'
+
+/**
+ * ContentAlign type.
+ */
+export type ContentAlign = 'top' | 'center' | 'bottom'
+
+/**
+ * Content props.
+ */
+export type ContentProps = {
+  /**
+   * children.
+   */
+  children?: React.ReactNode
+
+  /**
+   * content justify.
+   */
+  justify?: ContentJustify
+
+  /**
+   * content align.
+   */
+  align?: ContentAlign
+
+  /**
+   * content height.
+   */
+  height?: string
+
+  /**
+   * full width.
+   */
+  fullWidth?: boolean
+}
+
+/**
+ * Content component.
+ */
+export const Content: React.VFC<ContentProps> = (props) => {
+  return (
+    <>
+      <div className="content">{props.children}</div>
+
+      <style jsx>{`
+        .content {
+          display: flex;
+          justify-content: ${props.justify ?? 'left'};
+          align-items: ${props.align ?? 'top'};
+          flex-direction: column;
+
+          ${!props.fullWidth && 'max-width: 1200px;'}
+          width: 100%;
+          ${props.height && `height: ${props.height};`}
+        }
+      `}</style>
+    </>
+  )
+}
