1f777c1770811eacffa2bba36e524961f9288923
= <hotalog@hota1024.com>
2021-11-11 18:21:01 +0900

    🍱using Noto Sans JP font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,22 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@500&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
       <Component {...pageProps} />
 
       <style global jsx>{`
@@ -11,6 +25,10 @@ function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
           padding: 0;
           margin: 0;
           background: #fefefe;
+
+          font-family: 'Noto Sans JP', sans-serif;
+
+          color: #202020;
         }
       `}</style>
     </>
