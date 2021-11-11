fa80c330418f2096bd44588002183020509e0607
= <hotalog@hota1024.com>
2021-11-11 16:49:27 +0900

    🎉Init

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+/**
+ * HomePage component.
+ */
+export const HomePage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>home - yandereca.tech</title>
+      </Head>
+
+      <div>yandereca.tech</div>
+    </>
+  )
+}
+
+export default HomePage
